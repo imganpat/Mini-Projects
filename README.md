2c4f77cebbabe82effb992d79b469e2a4a6b0c08
Ganpat <coolganpat41@gmail.com>
2024-05-13 21:55:26 +0530

    created a homepage for all projects

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mini Projects
+
+This repository consists of small projects build with html, css and js to create a strong base on basic of web development. 
+
+### Note: No libraries or framework is used in any of the projects all uses raw html, css and js.
+
+## The main focus of this repository is learning.
---
+++ script.js
@@ -0,0 +1,121 @@
+const projectData = [
+    {
+        id: 1,
+        name: "Animation Controller",
+        image: "screenshots/01_animaton_controller.png",
+        demo: "https://animation-controller.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/01%20Animation%20Controler"
+    },
+    {
+        id: 2,
+        name: "Modern Shoe Card",
+        image: "screenshots/02_shoe_card.png",
+        demo: "https://modern-shoe-card.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/02%20Modern%20Shoe%20Card"
+    },
+    {
+        id: 3,
+        name: "Drum Kit",
+        image: "screenshots/03_drum.png",
+        demo: "https://drum-band-kit.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/03%20Drum"
+    },
+    {
+        id: 4,
+        name: "Toggle Theme Button",
+        image: "screenshots/04_theme_toggle.png",
+        demo: "https://toggle-theme-btn.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/04%20Theme%20Toggle"
+    },
+    {
+        id: 5,
+        name: "Digital Clock",
+        image: "screenshots/05_digital_clock.png",
+        demo: "https://digital-clock-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/05%20Digital%20Clock"
+    },
+    {
+        id: 6,
+        name: "Form validation",
+        image: "screenshots/06_form_validation.png",
+        demo: "https://form-validation-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/06%20Form%20Validation"
+    },
+    {
+        id: 7,
+        name: "Profile Card",
+        image: "screenshots/07_profile.png",
+        demo: "https://profile-card-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/07%20Profile%20Card"
+    },
+    {
+        id: 8,
+        name: "Dice",
+        image: "screenshots/08_dice.png",
+        demo: "https://dice-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/08%20Dice"
+    },
+    {
+        id: 9,
+        name: "Palindrome Checker",
+        image: "screenshots/09.png",
+        demo: "https://palindrome-checker-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/09%20Palindrome%20Checker",
+    },
+    {
+        id: 10,
+        name: "Readibility Analyzer",
+        image: "screenshots/10.png",
+        demo: "https://readibility-analyzer.netlify.app/",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/10%20Readibility%20Analyzer",
+    },
+    {
+        id: 11,
+        name: "Traffic Light",
+        image: "screenshots/11.png",
+        demo: "https://traffic-light-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/11%20Traffic%20Light",
+    },
+    {
+        id: 12,
+        name: "Animated Backgound",
+        image: "screenshots/12.png",
+        demo: "https://animated-background-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/12%20Animated%20Background",
+    },
+    {
+        id: 13,
+        name: "Sidebar UI",
+        image: "screenshots/13.png",
+        demo: "https://sidebar-gc.netlify.app",
+        code: "https://github.com/imganpat/Mini-Projects/tree/main/13%20Sidebar",
+    },
+
+];
+
+const container = document.querySelector(".container");
+
+projectData.forEach(project => {
+    const card = document.createElement("a");
+
+    card.innerText = project.name;
+    card.setAttribute("href", project.demo)
+    card.setAttribute("target", "_blank")
+
+    card.innerHTML = `<div class = "project-card" title = "Preview">
+                        <div class = "project-image">
+                            <img src = ${project.image} />
+                        </div>
+                        <div class = "project-desc">
+                            <div class = "project-name">${project.id + ". " + project.name}</div>
+                            <div class ="project-code">
+                                <a href = ${project.code} target = "_blank">
+                                    <i class = "fa-brands fa-github" title = "Source Code"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>`
+
+    container.appendChild(card)
+
+})
